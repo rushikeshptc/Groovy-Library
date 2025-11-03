51a847d1bedbe6b52ff11042d3e81a1a01c09303
Wayal <rwayal@ptc.com>
2025-11-03 16:49:22 +0530

    Add debugging information to info function

--- vars/info.groovy
+++ vars/info.groovy
@@ -1,5 +1,8 @@
 // vars/info.groovy
 
 def call() {
+    echo "=== INFO FUNCTION CALLED ==="
     echo "This is an info message from shared library!"
+    echo "Function executed successfully at: ${new Date()}"
+    echo "=== END INFO FUNCTION ==="
 }
