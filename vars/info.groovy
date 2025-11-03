9ffda3e7439b7b49d3b95f3977ffc94e4472b1d3
Wayal <rwayal@ptc.com>
2025-11-03 16:53:42 +0530

    Remove Date() function that might cause serialization issues

--- vars/info.groovy
+++ vars/info.groovy
@@ -3,6 +3,6 @@
 def call() {
     echo "=== INFO FUNCTION CALLED ==="
     echo "This is an info message from shared library!"
-    echo "Function executed successfully at: ${new Date()}"
+    echo "Function executed successfully!"
     echo "=== END INFO FUNCTION ==="
 }
